33cb2bc66be3459f79d7cd12385e7104793196a2
kyeongchan92 <nahcklee@gmail.com>
2023-09-07 23:46:49 +0900

    sasrec 추론결과 보임

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from config.views import home
@@ -22,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home)
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- config/views.py
+++ config/views.py
@@ -1,5 +1,25 @@
+import pandas as pd
+import torch
 from django.shortcuts import render
+import numpy as np
+from pytorch_models.sasrec.args import args
+from pytorch_models.sasrec.sasrec import SASRec
+
+sasrec = SASRec(6040, 3416, args)
+sasrec.load_state_dict(torch.load('pytorch_models/sasrec/sasrec.pth'))
+sasrec.eval()
+
+# movie_dictionary
+movies = pd.read_table('data/ml-1m/movies.dat', sep='::', header=None, names=['movie_id', 'title', 'genres'],
+                       engine='python', encoding_errors='ignore')
+movies.set_index('movie_id', inplace=True)
+movie_dict = movies.to_dict('index')
 
 
 def home(request):
-    return render(request, "home.html")
+    # print(f"request.POST : {request.POST['watched_movie']}")
+    result = sasrec.predict(log_seqs=np.array([[1, 2, 3]]), item_indices=np.array([[1, 2, 3, 4]]))
+    result = result.detach().numpy()[0]
+    print(f'result : {result}')
+    context = {'result': result}
+    return render(request, "home.html", context=context)

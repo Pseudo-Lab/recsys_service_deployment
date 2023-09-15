83a165008c2dd4098970d4fde85484a01533c6c3
kyeongchan92 <nahcklee@gmail.com>
2023-09-15 12:28:10 +0900

    movie app 생성

--- config/views.py
+++ config/views.py
@@ -15,6 +15,8 @@ movies = pd.read_table('data/ml-1m/movies.dat', sep='::', header=None, names=['m
 movies.set_index('movie_id', inplace=True)
 movie_dict = movies.to_dict('index')
 
+# model_dict{'sasrec' : sasrec}
+# model = model_dict['sasrec']
 
 def home(request):
     if request.method == "POST":
@@ -22,6 +24,7 @@ def home(request):
         watched_movie = request.POST['watched_movie']
         logits = sasrec.predict(log_seqs=np.array([[int(wm) for wm in watched_movie.split()]]),
                                 item_indices=[list(range(sasrec.item_emb.weight.size()[0]))])
+
         topk = 20
         result = logits.detach().cpu().numpy()[0].argsort()[::-1][:topk]
         context = {'result': [movie_dict[_] for _ in result]}

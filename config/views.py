b834b3c86d1bb74af78ed1a98a930d3083db1f98
이경찬B <nahcklee@hanatour.com>
2023-09-21 23:32:35 +0900

    입력창 위치 옮기기

--- config/views.py
+++ config/views.py
@@ -2,6 +2,8 @@ import pandas as pd
 import torch
 from django.shortcuts import render
 import numpy as np
+
+from movie.models import WatchedMovie
 from pytorch_models.sasrec.args import args
 from pytorch_models.sasrec.sasrec import SASRec
 
@@ -14,7 +16,7 @@ movies = pd.read_table('data/ml-1m/movies.dat', sep='::', header=None, names=['m
                        engine='python', encoding_errors='ignore')
 movies.set_index('movie_id', inplace=True)
 movie_dict = movies.to_dict('index')
-
+title_dict = {v['title']:k for k, v in movie_dict.items()}
 # model_dict{'sasrec' : sasrec}
 # model = model_dict['sasrec']
 
@@ -22,14 +24,28 @@ def home(request):
     if request.method == "POST":
         print("method POST")
         watched_movie = request.POST['watched_movie']
-        logits = sasrec.predict(log_seqs=np.array([[int(wm) for wm in watched_movie.split()]]),
+        print(f"watched_movie : {watched_movie}")
+        split = [int(wm) for wm in watched_movie.split()]
+        # watched_id = title_dict[watched_movie]
+        WatchedMovie.objects.create(name=watched_movie)
+        print(f"WatchedMovie.objects.all() : {WatchedMovie.objects.all()}")
+        split = [1, 2, 3, 4]
+        movie_names = [movie_dict[movie_id]['title'] for movie_id in split]
+        print(f"movie_names : {movie_names}")
+
+
+
+        logits = sasrec.predict(log_seqs=np.array([split]),
                                 item_indices=[list(range(sasrec.item_emb.weight.size()[0]))])
 
         topk = 20
-        result = logits.detach().cpu().numpy()[0].argsort()[::-1][:topk]
-        context = {'result': [movie_dict[_] for _ in result]}
+        recomm_result = logits.detach().cpu().numpy()[0].argsort()[::-1][:topk]
+        context = {
+            'recomm_result': [movie_dict[_] for _ in recomm_result],
+             'watched_movie' : movie_names
+         }
     else:
-        result = ['회원의', '이전 접속', '장바구니', '영화 기반', '추천결과']
-        print(f'result : {result}')
-        context = {'result': result}
+        recomm_result = ['회원의', '이전 접속', '장바구니', '영화 기반', '추천결과']
+        print(f'recomm_result : {recomm_result}')
+        context = {'recomm_result': recomm_result}
     return render(request, "home.html", context=context)

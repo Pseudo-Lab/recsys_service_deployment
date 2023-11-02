af9f55b6fc427990bbe097824280a5b1e195ac71
kyeongchan92 <nahcklee@gmail.com>
2023-11-02 21:58:02 +0900

    225p까지 진행함

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import login_view, logout_view
+from users.views import login_view, logout_view, signup
 
 urlpatterns = [
     path("login/", login_view),
     path("logout/", logout_view),
+    path("signup/", signup),
 ]

8c27d996274354f9b6f5df7bf191df3c18198980
kyeongchan92 <nahcklee@gmail.com>
2023-11-26 03:03:12 +0900

    별점 완성. view에서 받아와야함

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,6 @@ import json
 import time
 
 import numpy as np
-import torch
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
@@ -10,14 +9,7 @@ from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
 from clients import MysqlClient, DynamoDB
-from movie.models import WatchedMovie
 from predict import Predictor
-from pytorch_models.sasrec.args import args
-from pytorch_models.sasrec.sasrec import SASRec
-
-sasrec = SASRec(6040, 3416, args)
-sasrec.load_state_dict(torch.load('pytorch_models/sasrec/sasrec.pth'))
-sasrec.eval()
 
 # movie_dictionary
 # movies = pd.read_table('data/ml-1m/movies.dat', sep='::', header=None, names=['movie_id', 'title', 'genres'],
@@ -44,6 +36,7 @@ table_clicklog = DynamoDB(table_name='clicklog')
 
 predictor = Predictor()
 
+
 # model_dict{'sasrec' : sasrec}
 # model = model_dict['sasrec']
 
@@ -58,17 +51,7 @@ def home(request):
         watched_movie = request.POST['watched_movie']
         print(f"watched_movie : {watched_movie}")
         split = [int(wm) for wm in watched_movie.split()]
-        # watched_id = title2id[watched_movie]
-        # WatchedMovie.objects.create(name=watched_movie)
-        # print(f"WatchedMovie.objects.all() : {WatchedMovie.objects.all()}")
-        # split = [1, 2, 3, 4]
-        # print(f"movie_names : {movie_names}")
-
-        logits = sasrec.predict(log_seqs=np.array([split]),
-                                item_indices=[list(range(sasrec.item_emb.weight.size()[0]))])
-
-        topk = 20
-        recomm_result = logits.detach().cpu().numpy()[0].argsort()[::-1][:topk]
+        recomm_result = predictor.predict(model_name='sasrec', input_item_ids=split)
         context = {
             'recomm_result': [movie_dict[_] for _ in recomm_result],
             'watched_movie': [movie_dict[movie_id]['title'] for movie_id in split]
@@ -81,7 +64,6 @@ def home(request):
             print(f"user_df : \n{user_df}")
             clicked_movie_ids = [title2id[_] for _ in user_df['title'].tolist()]
             recomm_result = predictor.predict(model_name='sasrec', input_item_ids=clicked_movie_ids)
-            print(f"recomm_result : {recomm_result}")
             watched_movie_titles = [movie_dict[movie_id]['title'] for movie_id in clicked_movie_ids]
             context = {
                 'pop_movies': pop_movies,
@@ -93,7 +75,7 @@ def home(request):
             print(f"No POST request!")
             # print(f"watched_movie_titles : {watched_movie_titles}")
             context = {
-                'pop_movies' : pop_movies,
+                'pop_movies': pop_movies,
                 # 'recomm_result': [movie_dict[_] for _ in pop_movies],
                 # 'watched_movie': watched_movie_titles
             }
@@ -112,6 +94,7 @@ def log_click(request):
         print(f"\tL user : {request.user}")
         data = json.loads(request.body.decode('utf-8'))
         movie_title = data.get('movie_title')
+        page_url = data.get('page_url')
 
         if not request.session.session_key:
             request.session['init'] = True
@@ -121,13 +104,14 @@ def log_click(request):
 
         # 터미널에 로그 출력
         print(f"\tL Movie clicked: {movie_title}")
+        print(f"\tL url: {page_url}")
 
         # Kafka Producer 생성 프로듀서는 데이터 저장
         producer = KafkaProducer(bootstrap_servers='localhost:9092',
                                  value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
         # 클릭 로그를 Kafka topic에 전송
-        message = {'movie_title': movie_title, 'session_id': session_id}
+        message = {'movie_title': movie_title, 'session_id': session_id, 'url': page_url}
         producer.send('movie_title_ver2', message)
         producer.flush()
         producer.close()
@@ -135,10 +119,10 @@ def log_click(request):
         # dynamoDB clicklog 테이블에 저장 ######################
         click_log = {
             'userId': request.user.username,
+            'timestamp': int(time.time()),
             'title': movie_title,
-            'timestamp': int(time.time())
+            'url': page_url
         }
-        print(request.user.username)
         table_clicklog.put_item(click_log=click_log)
         ####################################################
 
--- static/js/click_logger.js
+++ static/js/click_logger.js
@@ -2,12 +2,14 @@
 $(document).ready(function(){
     $("img").click(function(){
         var movie_title = $(this).next("h2").text();
+        var page_url = window.location.href;
         $.ajax({
             type: "POST",
             url: "/log_click/",
             contentType: "application/json;charset=utf-8",
             data: JSON.stringify({
                 'movie_title': movie_title,
+                'page_url' : page_url
             }),
             dataType: "json",
             success: function(data) {

c8e35be49f94493d0183b76f98c923ca97198335
kyeongchan92 <nahcklee@gmail.com>
2023-12-03 12:06:43 +0900

    daum 크롤링 코드

--- predict.py
+++ predict.py
@@ -19,7 +19,7 @@ class Predictor:
 
     def predict(self, model_name: str, input_item_ids: List):
         model = self.models[model_name]
-        logits = model.predict(log_seqs=np.array([input_item_ids]),
+        logits = model.predict(log_seqs=np.array([input_item_ids][:50]),
                                item_indices=[list(range(model.item_emb.weight.size()[0]))])
         recomm_result = logits.detach().cpu().numpy()[0].argsort()[::-1][:self.topk]
 

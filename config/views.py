80a6fc3108863e7f45804d2d995b54fde0573402
SoonHyeok PARK <73877159+soonhp@users.noreply.github.com>
2023-12-06 16:56:55 +0900

    Update views.py

--- config/views.py
+++ config/views.py
@@ -38,14 +38,6 @@ device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('
 
 all_items = [i for i in range(3706)]
 
-# 데이터 불러오기
-data = pd.read_table('data/ml-1m/ratings.dat',sep='::',header=None, names=['uid','iid','r','ts'],encoding='latin-1')
-
-user_encoder = {id:idx for idx, id in enumerate(data['uid'].unique())}
-user_decoder = {idx:id for id, idx in user_encoder.items()}
-
-item_encoder = {id:idx for idx, id in enumerate(data['iid'].unique())}
-item_decoder = {idx:id for id, idx in item_encoder.items()}
 
 # movie_dictionary
 movies = pd.read_table('data/ml-1m/movies.dat', sep='::', header=None, names=['movie_id', 'title', 'genres'],

c08e7ee1572fb93023e2b452d6926667d3242110
kyeongchan92 <nahcklee@gmail.com>
2024-01-18 21:32:41 +0900

    논문리뷰 CSS 수정

---
+++ movie/predictors/ngcf_predictor.py
@@ -0,0 +1,3 @@
+
+# class NgcfPredictor:
+
--- paper_review/admin.py
+++ paper_review/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Post
-
-admin.site.register(Post)
+from markdownx.admin import MarkdownxModelAdmin
+admin.site.register(Post, MarkdownxModelAdmin)
 

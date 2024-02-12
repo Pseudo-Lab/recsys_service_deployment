b6bae6081bb1ebf6585e6a2d6deab24532bc87c7
kyeongchan92 <nahcklee@gmail.com>
2024-02-12 20:55:48 +0900

    도커에 gunicorn 적용하기

--- config/urls.py
+++ config/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('llmrec/', include('llmrec.urls')),
     path('accounts/', include('allauth.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

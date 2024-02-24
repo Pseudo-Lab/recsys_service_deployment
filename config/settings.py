3af6191af300c55ce9a37d1e580113059f3b9e0b
kyeongchan92 <nahcklee@gmail.com>
2024-02-25 08:54:49 +0900

    검색창 만듦

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from movie.views import home, movie_detail
+from movie.views import home, movie_detail, search
 
 urlpatterns = [
     path("movierec/", home),
     path("movierec/.pop", home),
     path("movierec/.sasrec", home),
-    path("<int:movie_id>", movie_detail)
+    path("<int:movie_id>", movie_detail),
+    path("", search, name='search')
 ]

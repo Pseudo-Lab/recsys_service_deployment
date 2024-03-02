f65d2b1f7064a93779caa5a8a51e4e04220aca11
kyeongchan92 <nahcklee@gmail.com>
2024-03-02 16:25:51 +0900

    검색기능 추가 완료

--- config/urls.py
+++ config/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('about_us/', include('single_pages.urls')),
     path('llmrec/', include('llmrec.urls')),
     path('accounts/', include('allauth.urls')),
-    path('search/', include("movie.urls"))
+    # path('search/', include("movie.urls"))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

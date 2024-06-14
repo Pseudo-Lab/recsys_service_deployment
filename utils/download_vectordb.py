9d2a54a4d9cb4870e41ebb9526c6a876ee5c470c
kyeongchan92 <nahcklee@gmail.com>
2024-06-14 18:18:27 +0900

    현우님 파일 다운로드

--- utils/download_vectordb.py
+++ utils/download_vectordb.py
@@ -18,8 +18,11 @@ def download_vectordb():
         os.makedirs('llmrec/vector_dbs/hyeonwoo')
     if not os.path.exists('llmrec/vector_dbs/hyeonwoo/chroma.sqlite3'):
         print(f"Download Hyeonwoo's chromadb")
-        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/chroma.sqlite3',
-                         Filename='llmrec/vector_dbs/hyeonwoo/chroma.sqlite3')
+        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/chroma_db_content_0614/chroma.sqlite3', Filename='llmrec/vector_dbs/hyeonwoo/chroma.sqlite3')
+        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/chroma_db_title_0614/chroma.sqlite3', Filename='llmrec/vector_dbs/hyeonwoo/chroma.sqlite3')
+        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/dictionary/actor_rec.json', Filename='llmrec/vector_dbs/hyeonwoo/dictionary/actor_rec.json')
+        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/dictionary/director_rec.json', Filename='llmrec/vector_dbs/hyeonwoo/dictionary/director_rec.json')
+        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/dictionary/title_rec.json', Filename='llmrec/vector_dbs/hyeonwoo/dictionary/title_rec.json')
 
     if not os.path.exists('llmrec/vector_dbs/gyungah'):
         os.makedirs('llmrec/vector_dbs/gyungah')

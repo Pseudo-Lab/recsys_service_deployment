fef36ce3d29e810f785bbb72fd41a56213f9dd37
kyeongchan92 <nahcklee@gmail.com>
2024-06-14 15:23:18 +0900

    llm답변 로깅

--- llmrec/utils/log_questions.py
+++ llmrec/utils/log_questions.py
@@ -6,14 +6,15 @@ from movie.utils import get_username_sid
 table_llm = DynamoDBClient(table_name='llm')
 
 
-def log_question(request, question, model_name, _from=''):
+def log_llm(request, model_name, question='', answer='', _from=''):
     username, session_id = get_username_sid(request, _from=_from)
     log = {
         'userId': username,
         'sessionId': session_id,
         'model': model_name,
         'timestamp': int(time.time()),
-        'question': question
+        'question' : question,
+        'answer': answer
     }
     table_llm.put_item(click_log=log)
     return

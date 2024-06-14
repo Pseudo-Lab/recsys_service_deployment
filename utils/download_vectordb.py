1caafe281f77d89f3bbb980bb61d9371b5c3afbf
kyeongchan92 <nahcklee@gmail.com>
2024-06-14 15:02:12 +0900

    chromadb 다운로드 메시지

--- utils/download_vectordb.py
+++ utils/download_vectordb.py
@@ -7,7 +7,7 @@ load_dotenv('.env.dev')
 
 
 def download_vectordb():
-    print(f"Download chroma.sqlite3".ljust(60, '>'))
+    print(f"Download chroma.sqlite3".ljust(60, '-'))
     s3 = boto3.client(
         service_name="s3",
         region_name="ap-northeast-2",
@@ -17,12 +17,16 @@ def download_vectordb():
     if not os.path.exists('llmrec/vector_dbs/hyeonwoo'):
         os.makedirs('llmrec/vector_dbs/hyeonwoo')
     if not os.path.exists('llmrec/vector_dbs/hyeonwoo/chroma.sqlite3'):
-        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/chroma.sqlite3', Filename='llmrec/vector_dbs/hyeonwoo/chroma.sqlite3')
+        print(f"Download Hyeonwoo's chromadb")
+        s3.download_file(Bucket='pseudorec-data', Key='hyeonwoo/chroma.sqlite3',
+                         Filename='llmrec/vector_dbs/hyeonwoo/chroma.sqlite3')
 
     if not os.path.exists('llmrec/vector_dbs/gyungah'):
         os.makedirs('llmrec/vector_dbs/gyungah')
     if not os.path.exists('llmrec/vector_dbs/hyeonwoo/chroma.sqlite3'):
-        s3.download_file(Bucket='pseudorec-data', Key='gyungah/chroma.sqlite3', Filename='llmrec/vector_dbs/gyungah/chroma.sqlite3')
+        print(f"Download Gyungah's chromadb")
+        s3.download_file(Bucket='pseudorec-data', Key='gyungah/chroma.sqlite3',
+                         Filename='llmrec/vector_dbs/gyungah/chroma.sqlite3')
 
 
 if __name__ == "__main__":

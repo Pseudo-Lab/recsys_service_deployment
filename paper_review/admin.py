c53cac9b77b9cf5f2858687b6d5f988e9e7b33ba
soonhp <tmxk0712@naver.com>
2024-09-11 11:59:26 +0900

    삭제된 커밋 살리기

--- paper_review/admin.py
+++ paper_review/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, PostMonthlyPseudorec
+from .models import Post, PostMonthlyPseudorec, Comment
 from markdownx.admin import MarkdownxModelAdmin
 admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(PostMonthlyPseudorec, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
